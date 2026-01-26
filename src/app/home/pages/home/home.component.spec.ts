9871fc894ce68787891b283b237796d63c855b2b
Flavio Moreno <flaviomoreno321@gmail.com>
2026-01-25 21:18:59 -0300

    fix: update CI workflow to use npm install instead of npm ci and enhance HomeComponent tests with HTTP client and environment provider

--- src/app/home/pages/home/home.component.spec.ts
+++ src/app/home/pages/home/home.component.spec.ts
@@ -1,13 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ENVIRONMENT } from 'src/app/config/environment.token';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [
+        { provide: ENVIRONMENT, useValue: { apiUrl: 'http://localhost:5047', production: false } },
+        provideHttpClient()
+      ]
     })
       .compileComponents();
 
